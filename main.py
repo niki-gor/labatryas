f9f957ee6ea9aa915d84975a115f7fde75aca543
Nikita Goryachev <nic.gor@yandex.ru>
2023-01-01 13:10:27 +0300

    Optimize imports

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import logging
 import time
 
 import aiogram.utils.markdown as md
-from aiogram import Bot, Dispatcher
-from aiogram import types
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
